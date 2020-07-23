9657e2a87f68fdebdf073680e38bbb08cc3f1d07
Stephen <stephen.sjbrown@gmail.com>
2020-07-23 13:43:55 +0100

    add additional shop/cart functionality

--- frontend/shop.js
+++ frontend/shop.js
@@ -1,24 +1,37 @@
-$.ajax({
-  url: "http://localhost:8000/api/1",
-  method: "GET",
-  success: function (item) {
-    $(`#item-name`)[0].innerText = item.item_name;
-    $(`#item-colour`)[0].innerText = item.colour;
-    $(`#item-price`)[0].innerText = `£${item.value}`;
-    $(`#item-description`)[0].innerText = item.description;
-    $(`#small-quantity-range`)[0].max = item.s_quantity;
-    $(`#medium-quantity-range`)[0].max = item.m_quantity;
-    $(`#large-quantity-range`)[0].max = item.l_quantity;
-    $(`#x-large-quantity-range`)[0].max = item.xl_quantity;
-    console.log(`got data`);
-  },
-  error: function (error) {
-    console.log(`Error: ${error}`);
-  },
-});
+// HIT THAT API
+// $.ajax({
+//   url: "http://localhost:8000/api/1",
+//   method: "GET",
+//   success: function (item) {
+//     $(`#item-name`)[0].innerText = item.item_name;
+//     $(`#item-colour`)[0].innerText = item.colour;
+//     $(`#item-price`)[0].innerText = `£${item.value}`;
+//     $(`#item-description`)[0].innerText = item.description;
+//     $(`#small-quantity-range`)[0].max = item.s_quantity;
+//     $(`#medium-quantity-range`)[0].max = item.m_quantity;
+//     $(`#large-quantity-range`)[0].max = item.l_quantity;
+//     $(`#x-large-quantity-range`)[0].max = item.xl_quantity;
+//     console.log(`got data`);
+//   },
+//   error: function (error) {
+//     console.log(`Error: ${error}`);
+//   },
+// });
 
-console.log(sessionStorage)
-$(`#cart-counter`)[0].innerText = sessionStorage.getItem("item total")
+if (sessionStorage.getItem("item total")) {
+  $(`#cart-counter`)[0].innerText = sessionStorage.getItem("item total")
+  $(`#cart-counter`).show()
+}
+else {
+  sessionStorage.setItem("item total", 0)
+}
+
+$(`.range-selector`).each((i, option) => {
+if (option.max === "0") {
+  option.type = "hidden"
+  option.value = "Sold Out"
+}
+})
 
 const showSize = (e) => {
   let allSizes = $(`.range-selector`);
@@ -27,6 +40,12 @@ const showSize = (e) => {
   shownItem.show();
   let quantity = $(`#quantity-value`)[0];
   quantity.innerHTML = shownItem[0].value;
+  if (shownItem[0].max === "0") {
+    $(`#add-to-cart`).hide()
+  }
+  else {
+    $(`#add-to-cart`).show()
+  }
 };
 
 const changeValue = (e) => {
@@ -35,11 +54,26 @@ const changeValue = (e) => {
 };
 
 const addToCart = () => {
-  let count = $(`#cart-counter`)[0]
-  let newCount = $(`#quantity-value`)[0].innerText
-  count.innerHTML = parseInt(newCount, 10) + parseInt(count.innerHTML)
-  sessionStorage.setItem("item total", parseInt(count.innerText))
-  console.log(sessionStorage)
+  let cartCount = $(`#cart-counter`)[0]
+  let quantity = $(`#quantity-value`)[0].innerText
+  cartCount.innerHTML = parseInt(quantity, 10) + parseInt(cartCount.innerHTML)
+  sessionStorage.setItem("item total", parseInt(cartCount.innerText))
+  $(`#cart-counter`).show()
+  const urlParams = new URLSearchParams(window.location.search)
+  let id = urlParams.get("item")
+  let name = $(`#item-name`)[0].innerText
+  let colour = $(`#item-colour`)[0].innerText
+  let size = $(`#size-selector`)[0].value
+  let price = $(`#item-price`)[0].innerHTML
+  let addition = {id, name, colour, price, size, quantity}
+  let existingOrder = JSON.parse(sessionStorage.getItem('order'))
+  if (existingOrder && existingOrder.id == id ) {
+    console.log("we got a match")
+    existingOrder.quantity = parseInt(existingOrder.quantity) + parseInt(quantity)
+    addition = existingOrder
+  }
+  sessionStorage.setItem("order", JSON.stringify(addition))
+  console.log(sessionStorage.getItem("order"))
 };
 
 let sizeSelector = $(`#size-selector`)[0];
