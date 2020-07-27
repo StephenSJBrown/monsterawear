ac79f77ebdb0e825427359766ae073f07567e6c0
Stephen <stephen.sjbrown@gmail.com>
2020-07-27 16:35:23 +0100

    add disappear/reappear for increment buttons

--- frontend/shop.js
+++ frontend/shop.js
@@ -1,3 +1,8 @@
+let small = 4;
+let medium = 8;
+let large = 5;
+let xl = 0;
+
 // HIT THAT API
 // $.ajax({
 //   url: "http://localhost:8000/api/1",
@@ -18,24 +23,42 @@
 //   },
 // });
 
-console.log(sessionStorage)
+console.log(sessionStorage);
 
-if (parseInt((sessionStorage.getItem("item count"))) > 0) {
-  $(`#cart-counter`)[0].innerText = sessionStorage.getItem("item count")
-  $(`#cart-counter`).show()
-}
-else {
-  sessionStorage.setItem("item count", 0)
+if (parseInt(sessionStorage.getItem("item count")) > 0) {
+  $(`#cart-counter`)[0].innerText = sessionStorage.getItem("item count");
+  $(`#cart-counter`).show();
+} else {
+  sessionStorage.setItem("item count", 0);
 }
 
 $(`.range-selector`).each((i, option) => {
-if (option.max === "0") {
-  option.type = "hidden"
-  option.value = "Sold Out"
-}
-})
+  if (option.max === "0") {
+    option.type = "hidden";
+    option.value = "Sold Out";
+  }
+});
 
 const showSize = (e) => {
+  switch (e.target.value) {
+    case "small":
+      console.log("change that bitch to small");
+      $("#quantity-number")[0].max = small;
+      break;
+    case "medium":
+      console.log("change that bitch to medium");
+      $("#quantity-number")[0].max = medium;
+      break;
+    case "large":
+      console.log("change that bitch to large");
+      $("#quantity-number")[0].max = large;
+      break;
+    case "x-large":
+      console.log("change that bitch to xl");
+      $("#quantity-number")[0].max = xl;
+      break;
+  }
+  $("#quantity-number")[0].value = 1;
   let allSizes = $(`.range-selector`);
   allSizes.hide();
   let shownItem = $(`#${e.target.value}-quantity-range`);
@@ -43,26 +66,32 @@ const showSize = (e) => {
   let quantity = $(`#quantity-value`)[0];
   quantity.innerHTML = shownItem[0].value;
   if (shownItem[0].max === "0") {
-    $(`#add-to-cart`).hide()
-  }
-  else {
-    $(`#add-to-cart`).show()
+    $(`#add-to-cart`).hide();
+  } else {
+    $(`#add-to-cart`).show();
   }
 };
 
-const increase = () => {
+const increase = (e) => {
   let quantity = $(`#quantity-number`)[0];
   if (parseInt(quantity.value) < parseInt(quantity.max)) {
-    quantity.value = parseInt(quantity.value) + 1
+    quantity.value = parseInt(quantity.value) + 1;
   }
-}
+  if (parseInt(quantity.value) === parseInt(quantity.max)) {
+    $(`#increase`)[0].style.visibility = "hidden";
+  }
+  $(`#decrease`)[0].style.visibility = "initial"
+};
 const decrease = () => {
   let quantity = $(`#quantity-number`)[0];
   if (parseInt(quantity.value) > 1) {
-    quantity.value = parseInt(quantity.value) - 1
+    quantity.value = parseInt(quantity.value) - 1;
   }
-}
-
+  if (parseInt(quantity.value) === parseInt(quantity.min)) {
+    $(`#decrease`)[0].style.visibility = "hidden";
+  }
+  $(`#increase`)[0].style.visibility = "initial"
+};
 
 const changeValue = (e) => {
   let quantity = $(`#quantity-value`)[0];
@@ -71,66 +100,68 @@ const changeValue = (e) => {
 
 const checkMatch = () => {
   // console.log("checking for match")
-  let result = false
-  let existingOrder = JSON.parse(sessionStorage.getItem('order'))
-  const urlParams = new URLSearchParams(window.location.search)
-  let id = urlParams.get("item")
-  let size = $(`#size-selector`)[0].value
-  let i = 0
+  let result = false;
+  let existingOrder = JSON.parse(sessionStorage.getItem("order"));
+  const urlParams = new URLSearchParams(window.location.search);
+  let id = urlParams.get("item");
+  let size = $(`#size-selector`)[0].value;
+  let i = 0;
   // console.log(existingOrder)
   if (existingOrder) {
-  for (item of existingOrder) {
-    // console.log("looping through order items")
-    // console.log(item)
-    if (existingOrder && item.id == id && item.size == size) {
-      result = true
-      // console.log(`one of the items, ${i} matches`)
-      return [result, i]
+    for (item of existingOrder) {
+      // console.log("looping through order items")
+      // console.log(item)
+      if (existingOrder && item.id == id && item.size == size) {
+        result = true;
+        // console.log(`one of the items, ${i} matches`)
+        return [result, i];
+      }
+      i++;
     }
-    i++
   }
-  }
-  return result
-}
+  return result;
+};
 
 const addToCart = () => {
   // add and show cart number
-  let cartCount = $(`#cart-counter`)[0]
-  let quantity = $(`#quantity-value`)[0].innerText
-  cartCount.innerHTML = parseInt(quantity, 10) + parseInt(cartCount.innerHTML)
-  sessionStorage.setItem("item count", parseInt(cartCount.innerText))
-  $(`#cart-counter`).show()
+  let cartCount = $(`#cart-counter`)[0];
+  let quantity = $(`#quantity-value`)[0].innerText;
+  cartCount.innerHTML = parseInt(quantity, 10) + parseInt(cartCount.innerHTML);
+  sessionStorage.setItem("item count", parseInt(cartCount.innerText));
+  $(`#cart-counter`).show();
   // check if there's a match within our existing order
-  const results = checkMatch()
+  const results = checkMatch();
   // console.log(`it is ${results[0]} there is a match. It is at ${results[1]}`)
   // if there is, just add current quantity to that item
   if (results[0]) {
     // console.log(`we got a match @ ${results[1]}`)
-    order = JSON.parse(sessionStorage.getItem("order"))
+    order = JSON.parse(sessionStorage.getItem("order"));
     // console.log(`quantity is ${order[results[1]].quantity}`)
-    const oldQuantity = parseInt(JSON.parse(sessionStorage.getItem("order"))[results[1]].quantity)
-    const newQuantity = oldQuantity + parseInt(quantity)
+    const oldQuantity = parseInt(
+      JSON.parse(sessionStorage.getItem("order"))[results[1]].quantity
+    );
+    const newQuantity = oldQuantity + parseInt(quantity);
     // console.log(oldQuantity, quantity, newQuantity)
-    order[results[1]].quantity = newQuantity
+    order[results[1]].quantity = newQuantity;
   }
   // if not, then create a new item and add it to the existing items
   else {
     // console.log(`no match`)
     // create new object
-    const urlParams = new URLSearchParams(window.location.search)
-    let id = urlParams.get("item")
-    let size = $(`#size-selector`)[0].value
-    let name = $(`#item-name`)[0].innerText
-    let colour = $(`#item-colour`)[0].innerText
-    let price = $(`#item-price`)[0].innerHTML
-    let addition = {id, name, colour, price, size, quantity}
-    order = []
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("item");
+    let size = $(`#size-selector`)[0].value;
+    let name = $(`#item-name`)[0].innerText;
+    let colour = $(`#item-colour`)[0].innerText;
+    let price = $(`#item-price`)[0].innerHTML;
+    let addition = { id, name, colour, price, size, quantity };
+    order = [];
     if (sessionStorage.getItem("order")) {
-      order = JSON.parse(sessionStorage.getItem("order"))
+      order = JSON.parse(sessionStorage.getItem("order"));
     }
-    order.push(addition)
+    order.push(addition);
   }
-  sessionStorage.setItem("order", JSON.stringify(order))
+  sessionStorage.setItem("order", JSON.stringify(order));
   // console.log(sessionStorage.getItem("order"))
 };
 
@@ -144,5 +175,5 @@ rangeSelectors.each((i, selector) => {
 
 $("#add-to-cart")[0].addEventListener("click", addToCart);
 
-$("#increase")[0].addEventListener("click", increase)
-$("#decrease")[0].addEventListener("click", decrease)
+$("#increase")[0].addEventListener("click", increase);
+$("#decrease")[0].addEventListener("click", decrease);
